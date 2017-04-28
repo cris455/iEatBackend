b142b76de3394f6daa68a8f30b7782786b46e52e
Cristian Perdomo <cristianeprdomo@unicauca.edu.co>
2017-04-28 05:32:25 -0500

    implementado login

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,25 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
 
+router.use((req, res, next) => {
+  req.collection = req.db.collection("users");
+  next();
+})
+
+router.post("/singin", (req, res, next) => {
+  let body = req.body;
+  req.collection.findOne({ usuario: body.usuario, contraseña: body.contraseña }).then(doc => {
+    if (doc) {
+      res.send({ success: true, user: doc });
+    } else {
+      res.send({ success: false });
+    }
+  }).catch(err => {
+    res.send({ success: false });
+  });
+});
 module.exports = router;
